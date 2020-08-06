d1d3665fe339035e372d1cf631cba9078180566c
Stuart Axon <stuaxo2@yahoo.com>
2020-08-06 15:03:25 +0100

    Bump version in setup.py and format with Black.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Should work on all platforms.
 report bugs to https://github.com/stuaxo/vext
 """
 
-version = "0.7.0"
+version = "0.7.4"
 vext_version = "vext>=%s" % version
 
 from glob import glob
@@ -37,7 +37,7 @@ class Install(install):
         #
         # TOOD - Move this code to a common place.
         print("vext.vtk Install")
-        if sys.prefix == '/usr':
+        if sys.prefix == "/usr":
             print("Not installing PTH file to real prefix")
             return
         call(["pip", "install", vext_version])
@@ -45,52 +45,53 @@ class Install(install):
         install.execute(self, _post_install, [self], msg="Install vext files:")
 
 
+# Disable wheels
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+    class bdist_wheel(_bdist_wheel):
+        def run(self):
+            return
+
+
+except ImportError:
+    bdist_wheel = None
+
+
 setup(
-    name='vext.vtk',
+    name="vext.vtk",
     version=version,
-    description='Use system VTK from a virtualenv',
+    description="Use system VTK from a virtualenv",
     long_description=info,
-
-    cmdclass={
-        'install': Install,
-    },
-
+    cmdclass={"install": Install,},
     # The project's main homepage.
-    url='https://github.com/stuaxo/vext',
-
+    url="https://github.com/stuaxo/vext",
     # Author details
-    author='Stuart Axon',
-    author_email='stuaxo2@yahoo.com',
-
+    author="Stuart Axon",
+    author_email="stuaxo2@yahoo.com",
     # Choose your license
-    license='MIT',
-
+    license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
     ],
-
     # What does your project relate to?
-    keywords='virtualenv vext vtk',
-
+    keywords="virtualenv vext vtk",
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     setup_requires=["setuptools>=0.18.8"],
     install_requires=[vext_version],
-    data_files=[('', vext_files)]
+    data_files=[("", vext_files)],
 )
